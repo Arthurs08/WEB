a0d766748690ec822e0b55ef7acaec740b26528c
Arthurs08 <arthur.sbruzzi@gmail.com>
2024-05-06 15:33:38 -0300

    java

--- Projetofinal_guanabara/Projetofinal_guanabara.java
+++ Projetofinal_guanabara/Projetofinal_guanabara.java
@@ -20,13 +20,26 @@ public class Projetofinal_guanabara {
         vis[0]= new Visualizacao(c[0], video[1]);
         vis[0].avaliar(87.0f);
         vis[1]= new Visualizacao(c[1], video[0]);
-        vis[1].avaliar();
+        vis[1].avaliar(2f);
+        vis[1].getFilme().like();
         vis[2]= new Visualizacao(c[2], video[2]);
+        vis[2].getFilme().like();
         vis[3]= new Visualizacao(c[0], video[1]);
         vis[3].avaliar(50.0f);
+        vis[3].getFilme().like();
         vis[4]= new Visualizacao(c[2], video[1]);
+
         for(int f= 0; f < vis.length; f++){
-            System.out.println(vis[f]);
+            System.out.println("--------Cliente--------");
+            System.out.println("Login..: " + vis[f].getEspectador().getLogin());
+            System.out.println("Idade..: " + vis[f].getEspectador().getIdade());
+            System.out.println("Sexo..: " + vis[f].getEspectador().getSexo());
+            System.out.println("Total filmes assistidos..: " + vis[f].getEspectador().getTotAssitido());
+            System.out.println("--------Filmes--------");
+            System.out.println("Filme..: " + vis[f].getFilme().getTitulo());
+            System.out.println("Visualização..: " + vis[f].getFilme().getViews());
+            System.out.println("Likes..: " + vis[f].getFilme().getCurtidas());
+            System.out.println("Avaliação..: " + vis[f].getFilme().getAvailiacao());
         }
         //for para mostrar conteudo de um vetor
    //     for(int v = 0; v < video.length; v++){
